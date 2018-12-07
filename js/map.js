a64fe61d5ffca2629d304067130e2782c8c18010
DamperX <gosu_1994@mail.ru>
2018-12-07 12:26:32 +0300

    Добавил пину событие drag and drop, отрисовал координаты пина

--- js/map.js
+++ js/map.js
@@ -267,7 +267,6 @@ var openPopup = function (evt) {
   document.addEventListener('keydown', pressEscClose);
 };
 
-
 mainPin.addEventListener('mouseup', function () {
   if (isAvailable) {
     showInterface();
@@ -276,8 +275,50 @@ mainPin.addEventListener('mouseup', function () {
   }
 });
 
-setAddress();
 hideInterface();
+
+mainPin.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    if (mainPin.offsetTop - shift.y < MAX_Y && mainPin.offsetTop - shift.y > MIN_Y) {
+      mainPin.style.top = (mainPin.offsetTop - shift.y) + 'px';
+    }
+    if (mainPin.offsetLeft - shift.x > MIN_X && mainPin.offsetLeft - shift.x < MAX_X) {
+      mainPin.style.left = (mainPin.offsetLeft - shift.x) + 'px';
+    }
+
+    adressInput.value = mainPin.offsetLeft + ', ' + mainPin.offsetTop;
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document. addEventListener('mouseup', onMouseUp);
+});
+
+setAddress();
 
 var inputTitle = adForms.querySelector('#title');
 var inputPrice = adForms.querySelector('#price');
