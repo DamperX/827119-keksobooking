58e05ad053a440d0c01ccac3105e9e4765d3f7eb
DamperX <gosu_1994@mail.ru>
2018-12-17 22:44:49 +0300

    Исправил ошибки eslint

--- js/filters.js
+++ js/filters.js
@@ -23,46 +23,46 @@
   var guestsSelect = mapFilters.querySelector('#housing-guests');
   var featuresCheckboxes = document.querySelectorAll('.map__checkbox');
 
-  var getTypeChange = function (realtors) {
+  var getTypeChange = function (realtor) {
     if (typeSelect.value === 'any') {
       return true;
     } else {
-      return realtors.offer.type === typeSelect.value;
+      return realtor.offer.type === typeSelect.value;
     }
   };
 
-  var getPriceChange = function (realtors) {
+  var getPriceChange = function (realtor) {
     switch (priceSelect.value) {
       case 'low':
-        return realtors.offer.price <= HousingPriceValue.low.maxPrice;
+        return realtor.offer.price <= HousingPriceValue.low.maxPrice;
       case 'middle':
-        return realtors.offer.price >= HousingPriceValue.middle.minPrice && realtors.offer.price <= HousingPriceValue.middle.maxPrice;
+        return realtor.offer.price >= HousingPriceValue.middle.minPrice && realtor.offer.price <= HousingPriceValue.middle.maxPrice;
       case 'heigh':
-        return realtors.offer.price >= HousingPriceValue.heigh.minPrice;
+        return realtor.offer.price >= HousingPriceValue.heigh.minPrice;
       default:
         return true;
     }
   };
 
-  var getRoomsChange = function (realtors) {
+  var getRoomsChange = function (realtor) {
     if (roomsSelect.value === 'any') {
       return true;
     }
 
-    return realtors.offer.rooms === Number(roomsSelect.value);
+    return realtor.offer.rooms === Number(roomsSelect.value);
   };
 
-  var getGuestsChange = function (realtors) {
+  var getGuestsChange = function (realtor) {
     if (guestsSelect.value === 'any') {
       return true;
     }
 
-    return realtors.offer.guests === Number(roomsSelect.value);
+    return realtor.offer.guests === Number(roomsSelect.value);
   };
 
-  var getFutureChange = function (realtors) {
+  var getFutureChange = function (realtor) {
     for (var i = 0; i < featuresCheckboxes.length; i++) {
-      if (featuresCheckboxes[i].checked && realtors.offer.features.indexOf(featuresCheckboxes[i].value) < 0) {
+      if (featuresCheckboxes[i].checked && realtor.offer.features.indexOf(featuresCheckboxes[i].value) < 0) {
         return false;
       }
     }
@@ -72,7 +72,7 @@
   var getFilterRealtors = function () {
     var realtorsCopy = window.realtorsList.slice();
     var filterdList = realtorsCopy.filter(function (realtor) {
-      return getTypeChange(realtors) && getPriceChange(realtors) && getRoomsChange(realtors) && getGuestsChange(realtors) && getFutureChange(realtors);
+      return getTypeChange(realtor) && getPriceChange(realtor) && getRoomsChange(realtor) && getGuestsChange(realtor) && getFutureChange(realtor);
     });
 
     window.map.closePopup();
