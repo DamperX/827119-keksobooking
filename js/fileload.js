9c02f73e773b58d7914cba208cf0b6ac629f36f3
DamperX <gosu_1994@mail.ru>
2018-12-19 13:22:54 +0300

    Добавил загрузку фотографий жилья

--- js/fileload.js
+++ js/fileload.js
@@ -7,13 +7,15 @@
   var inputAvatar = adForm.querySelector('#avatar');
   var avatarHeaderPrewview = adForm.querySelector('.ad-form-header__preview');
   var avatarPreview = adForm.querySelector('.ad-form-header__preview img');
-
-  var AvatarParameters = {
+  var inputImages = adForm.querySelector('#images');
+  var formPhoto = adForm.querySelector('.ad-form__photo');
+  var photoContainer = adForm.querySelector('.ad-form__photo-container');
+  var PhotoParameters = {
     width: '70',
     height: '70',
   };
 
-  var AVATAR_PADDING = 'padding: 0;';
+  var PHOTO_PADDING = 'padding: 0;';
 
   var loadAvatar = function () {
     var file = inputAvatar.files[0];
@@ -28,9 +30,36 @@
 
       reader.addEventListener('load', function () {
         avatarPreview.src = reader.result;
-        avatarPreview.width = AvatarParameters.width;
-        avatarPreview.height = AvatarParameters.height;
-        avatarHeaderPrewview.style = AVATAR_PADDING;
+        avatarPreview.width = PhotoParameters.width;
+        avatarPreview.height = PhotoParameters.height;
+        avatarHeaderPrewview.style = PHOTO_PADDING;
+      });
+
+      reader.readAsDataURL(file);
+    }
+  };
+
+  var loadPhoto = function () {
+    var file = inputImages.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = fileTypes.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        var photoWrapper = document.createElement('div');
+        photoWrapper.classList.add('ad-form__photo');
+
+        var photo = document.createElement('img');
+        photo.src = reader.result;
+        photo.width = PhotoParameters.width;
+        photo.height = PhotoParameters.height;
+        photoWrapper.appendChild(photo);
+        photoContainer.insertBefore(photoWrapper, formPhoto);
       });
 
       reader.readAsDataURL(file);
@@ -39,8 +68,13 @@
 
   window.resetLoad = function () {
     avatarPreview.src = 'img/muffin-grey.svg';
+    var formPhotos = photoContainer.querySelectorAll('.ad-form__photo');
+    for (var i = 0; i < formPhotos.length; i++) {
+      photoContainer.removeChild(formPhotos[i]);
+    }
   };
 
 
   inputAvatar.addEventListener('change', loadAvatar);
+  inputImages.addEventListener('change', loadPhoto);
 })();
